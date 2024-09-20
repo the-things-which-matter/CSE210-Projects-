be5131424ce7527dbae661321405db0321db6ab3
igabrigham <igabrigham9@gmail.com>
2024-09-20 10:41:14 +0100

    Finished my Prep 1 Project

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("This is my Prep 1 Project");
+
+        Console.Write("Please enter your First Name here. ");
+        string first_name = Console.ReadLine();
+        //Console.WriteLine(first_name);
+
+        Console.Write("Please enter your last name here ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+        Console.WriteLine("End of my pre 1 Projject");
+
+
+        
+
     }
+
 }
+
