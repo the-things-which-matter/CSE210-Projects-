8a570674d9d3d13c902211085f4530271fbe3d25
igabrigham <igabrigham9@gmail.com>
2024-09-20 15:58:09 +0100

    Changed Variable names to camel format

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -7,13 +7,13 @@ class Program
         Console.WriteLine("This is my Prep 1 Project");
 
         Console.Write("Please enter your First Name here. ");
-        string first_name = Console.ReadLine();
+        string firstName = Console.ReadLine();
         //Console.WriteLine(first_name);
 
         Console.Write("Please enter your last name here ");
-        string last_name = Console.ReadLine();
+        string lastName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
         Console.WriteLine("End of my pre 1 Projject");
 
 
