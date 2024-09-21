232aa6907b80c45880af59c50175cb0dc9b792d0
igabrigham <igabrigham9@gmail.com>
2024-09-21 10:03:15 +0100

    Finished Prep 3 Projject

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("This is my Prep 3 Project");
+
+        Random randomNumberGenerate = new Random();
+        int realNumber = randomNumberGenerate.Next(1,95);
+
+        int magicNumber = 0;
+
+        while (magicNumber != realNumber)
+            {
+                Console.Write("Enter youur guess ");
+                magicNumber = int.Parse(Console.ReadLine());
+
+                if (realNumber > magicNumber)
+                    {
+                        Console.WriteLine("Higher");
+
+                    }
+                else if (realNumber < magicNumber)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                else
+                {
+                    {
+                        Console.WriteLine("You guessed it");
+                    }
+                }
+            }
+        Console.WriteLine("End of Prep 3 Project");
+       
     }
 }
