6a2de59f5087d1caeabaf71de54eb447ec27b1d3
igabrigham <igabrigham9@gmail.com>
2024-10-03 14:31:07 +0100

    corrected console display code

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -8,13 +8,13 @@ class Program
     {
         string userChoice = "";
         string userFile ="";
+        //int userReply = 0;
 
         //Console.WriteLine("Hello Develop02 World!");
         //int userReturn = -2;
         //Random randomNumberGenerate = new Random();
         //string realNumber = randomNumberGenerate.Next("what frastrated you today","what made you smile","what made you think about life");
-        Console.WriteLine("Please Select onr of the following choices;");
-        string userReturn = Console.ReadLine();
+      
 
         PromptGenerator generate = new PromptGenerator();
         //while(userReturn != 5)
@@ -26,11 +26,11 @@ class Program
 
 
 
-        generate._prompts.Add("1.Write");
-        generate._prompts.Add("2.Display");
-        generate._prompts.Add("3.Load");
-        generate._prompts.Add("4.Save");
-        generate._prompts.Add("5.Quit");
+       // generate._prompts.Add("1.Write");
+        //generate._prompts.Add("2.Display");
+        //generate._prompts.Add("3.Load");
+        //generate._prompts.Add("4.Save");
+        //generate._prompts.Add("5.Quit");
 
         Entry newEntry = new Entry();
         newEntry.Display();
@@ -62,6 +62,18 @@ class Program
 
 
         Console.WriteLine(generate.GetRandomPrompt());
+
+        Console.WriteLine("Please Select onr of the following choices;");
+        Console.WriteLine("1.Write");
+        Console.WriteLine("2.Display");
+        Console.WriteLine("3.Load");
+        Console.WriteLine("4.Save");
+        Console.WriteLine("5.Quit");
+
+         
+
+        //string userReturn = Console.ReadLine();
+        
         string userReply = Console.ReadLine();
         int numberReply = int.Parse(userReply);
         while (numberReply != 5)
