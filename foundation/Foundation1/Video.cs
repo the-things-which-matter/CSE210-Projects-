c22e33cdb71cc1fb6cb695f2151be9cc3bd0ab25
igabrigham <igabrigham9@gmail.com>
2024-10-18 09:00:53 +0100

    Did the video assignment

--- foundation/Foundation1/Video.cs
+++ foundation/Foundation1/Video.cs
@@ -3,13 +3,13 @@ using System.Transactions;
 public class Video
     {
         private string _title;
-        private string _authour;
+        private string _author;
         private int _lengthInSeconds;
         private List<Comment> _comments = new List<Comment>();
         public Video(string title, string author, int legthInSecond)
             {
                 _title = title;
-                _authour = author;
+                _author = author;
                 _lengthInSeconds = legthInSecond;
             }
 
@@ -20,19 +20,27 @@ public class Video
 
         public int GetNumberOfComment()
             {
+                int place = 0;
+
                 foreach (Comment oneComment in _comments)
                     {
-                        int commentNumber = _comments.le
+                        place1 = oneComment[0]
+                        int commentNumber = place1
                     }
-                return;
+                return place1;
             }
         public string GetVideoDetails()
             {
+                Console.WriteLine($"The video was called {_title} and was written by {_author} and was {_lengthInSeconds}")
                 return "";
             }
 
         public void DisplayComments()
             {
+                foreach (Comment comment in _comments)
+                    {
+                        Console.WriteLine(comment);
+                    }
 
             }
 
